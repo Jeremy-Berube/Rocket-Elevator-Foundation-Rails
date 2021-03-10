8aa9d0950910bf357a47b874d4da5781089f56f4
Tatiana Ramirez <76738617+tatiana-wag@users.noreply.github.com>
2021-03-09 22:58:13 -0500

    Update elevator.rb
    little changes in slack message redaction

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -6,12 +6,12 @@ class Elevator < ApplicationRecord
     around_update :elevator_status_is_changed
     private
     def elevator_status_is_changed
-    notify = self.status_changed?
-    if notify
-        notifier = Slack::Notifier.new ENV['SLACK_API_TOKEN']
-        notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
-        notifier.ping "The elevator with ID '#{self.id} with serial number '#{self.serial_number} change status from '#{self.status_was} to '#{self.status}" 
-    end
-    yield
+        notify = self.status_changed?
+        if notify
+            notifier = Slack::Notifier.new ENV['SLACK_API_TOKEN']
+            notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+            notifier.ping "The elevator '#{self.id} with Serial Number '#{self.serial_number} changed status from '#{self.status_was} to '#{self.status}" 
+        end
+        yield
     end
 end
